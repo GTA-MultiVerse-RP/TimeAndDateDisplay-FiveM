5c47932731770bb0b6576a1d175fdac3b250fb89
gta multiverse rp <gta-multiverse-rp@fx.server>
2025-03-03 16:23:28 +0900

    導入

--- config.lua
+++ config.lua
@@ -2,11 +2,11 @@ Config = {}
 
 Config.ShowServerName = true -- Set true to show the given Config.ServerName below
 
-Config.ServerName = "Server Name" -- Shows this name if Config.ShowServerName = true
+Config.ServerName = "MultiVerseRP" -- Shows this name if Config.ShowServerName = true
 
 Config.ShowPlayerName = false -- Set true to show to player name
 
-Config.ShowPlayerID = false -- Set True to show to player id
+Config.ShowPlayerID = true -- Set True to show to player id
 
 -- One of the following config things below must be set to true
 
@@ -18,11 +18,11 @@ Config.ShowOnlyTime = false -- Set true to show only the Time
 
 -- One of the following config things below must be set to true
 
-Config.DayMonthYear = true -- Set true to have DD-MM-YYYY
+Config.DayMonthYear = false -- Set true to have DD-MM-YYYY
 
 Config.MonthDayYear = false -- Set true to have MM-DD-YYYY
 
-Config.YearMonthDay = false -- Set true to have YYYY-MM-DD
+Config.YearMonthDay = true -- Set true to have YYYY-MM-DD
 
 Config.YearDayMonth = false -- Set true to have YYYY-DD-MM
 
